2a7c4c5d72a25df9347177981ed130e3fb100abc
Angel Lizarzado <angel.lizarzado98@gmail.com>
2024-04-22 16:47:21 -0400

    Subiendo animaciones y añadiendo comentarios al html para mejorar la ubicacion

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ let navbar = document.querySelector('.header .navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
-    loginForm.classList.remove('active');
 }
+
+
